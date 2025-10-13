e48987a159ab633190da421073aaccca117fbd71
Boro-jangila <jangilaboro1999@gmail.com>
2025-10-13 23:23:11 +0530

    signals

---
+++ Day-9/01/circle.c
@@ -0,0 +1,10 @@
+#include"circle.h"
+#include<stdio.h>
+
+void c_area(void){
+
+	int r;
+	printf("Enter the radius :");
+	scanf("%d",&r);
+	printf("Area : %.2f\n",3.14*r*r);
+}
---
+++ Day-9/01/sq.c
@@ -0,0 +1,10 @@
+#include"sq.h"
+#include<stdio.h>
+
+void sq_area(void){
+
+	int a;
+	printf("Enter the side :");
+	scanf("%d",&a);
+	printf("Area : %d\n",a*a);
+}
